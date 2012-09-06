4dac9bdccb9bf16921684eb10ab09dcb6b241fb2
Max Kalus <max@beimax.de>
2012-09-06 09:56:36 +0200

    Revert changing background sounds - will make that customizable.

--- src/main/java/de/beimax/spacealert/mp3/BackgroundMP3Player.java
+++ src/main/java/de/beimax/spacealert/mp3/BackgroundMP3Player.java
@@ -61,6 +61,11 @@ public class BackgroundMP3Player extends Thread {
 	private Player player;
 	
 	/**
+	 * noise number reference - to differenciate a little bit
+	 */
+	private char noiseNumber = '0';
+	
+	/**
 	 * stop the thread completely
 	 */
 	public void stopThread() {
@@ -120,8 +125,10 @@ public class BackgroundMP3Player extends Thread {
 	 */
 	private void startMusicPlaying() {
 		try {
-			player = new Player(cache.getMP3InputStream("red_alert_2.mp3"));
+			player = new Player(cache.getMP3InputStream("red_alert_" + (noiseNumber++) + ".mp3"));
 			final Player myPlayer = player;
+			// reset noise if too high
+			if (noiseNumber >= '4') noiseNumber = '0';
 			// anonymous runnable
 			Runnable r = new Runnable() {
 				public void run() {
