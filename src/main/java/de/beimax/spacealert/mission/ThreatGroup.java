206ff998cd85468049d95f3bef7d1e385a4ad286
Alan Malloy <alan@malloys.org>
2012-09-09 15:55:17 -0600

    First draft of ThreatGroup container

---
+++ src/main/java/de/beimax/spacealert/mission/ThreatGroup.java
@@ -0,0 +1,53 @@
+package de.beimax.spacealert.mission;
+
+public class ThreatGroup {
+	private Threat internal = null;
+	private Threat external = null;
+	
+	public ThreatGroup() {
+	}
+
+	public Threat getInternal() {
+		return internal;
+	}
+
+	public Threat getExternal() {
+		return external;
+	}
+	
+	public boolean hasInternal() {
+		return internal != null;
+	}
+	
+	public boolean hasExternal() {
+		return external != null;
+	}
+	
+	public boolean addInternal(Threat i) {
+		if (internal == null) {
+			internal = i;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean addExternal(Threat e) {
+		if (external == null) {
+			external = e;
+			return true;
+		}
+		return false;
+	}
+	
+	public Threat removeInternal() {
+		Threat ret = internal;
+		internal = null;
+		return ret;
+	}
+	
+	public Threat removeExternal() {
+		Threat ret = external;
+		external = null;
+		return ret;
+	}
+}
